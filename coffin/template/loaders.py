f936deb1229ae14fa28eb3d92c99e238214bde7e
Jonathan Stoppani <jonathan@stoppani.name>
2012-03-11 15:10:13 +0100

    Updated templates exclusion method

--- coffin/template/loaders.py
+++ coffin/template/loaders.py
@@ -3,27 +3,49 @@ from django.template.loader import BaseLoader, find_template_loader, make_origin
 from django.template import TemplateDoesNotExist
 from coffin.template.loader import get_template
 from django.conf import settings
+import re
 
 django_template_source_loaders = None
 
 class Loader(BaseLoader):
     """
-    A template loader to be used 
+    A template loader to be used
     """
     is_usable = True
-    
+
+    def __init__(self, *args, **kwargs):
+        super(Loader, self).__init__(*args, **kwargs)
+
+        self._disabled = set()
+        self._enabled = set()
+
+        self._disabled_templates = set(getattr(settings, 'JINJA2_DISABLED_TEMPLATES', []))
+
+    def is_enabled(self, template_name):
+        if template_name in self._disabled:
+            return False
+        elif template_name in self._enabled:
+            return True
+        else:
+            # Check and update cache
+            for pattern in self._disabled_templates:
+                if re.match(pattern, template_name) is not None:
+                    self._disabled.add(template_name)
+                    return False
+            else:
+                self._enabled.add(template_name)
+                return True
+
     def load_template(self, template_name, template_dirs=None):
-        try:
-            if template_name.split('/', 1)[0] in settings.JINJA2_DISABLED_APPS:
-                return get_django_template(template_name, template_dirs)
-        except IndexError, AttributeError:
-            pass
-            
-        try:
-            template = get_template(template_name)
-        except TemplateNotFound:
-            raise TemplateDoesNotExist(template_name)
-        return template, template.filename
+        if self.is_enabled(template_name):
+            try:
+                template = get_template(template_name)
+            except TemplateNotFound:
+                raise TemplateDoesNotExist(template_name)
+            return template, template.filename
+        else:
+            return get_django_template(template_name, template_dirs)
+
 
 def get_django_template(name, dirs=None):
     global django_template_source_loaders
@@ -34,7 +56,7 @@ def get_django_template(name, dirs=None):
             if loader is not None:
                 loaders.append(loader)
         django_template_source_loaders = tuple(loaders)
-    
+
     for loader in django_template_source_loaders:
         try:
             source, display_name = loader(name, dirs)
